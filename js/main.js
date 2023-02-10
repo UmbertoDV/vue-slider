379b9a0ed98e537358531ff7023b1b69eb6d8bf3
UmbertoDV <biccoumby2002@hotmail.com>
2023-02-10 15:46:13 +0100

    for

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-"use strict";
+const { createApp } = Vue;
+
+createApp({
+	data() {
+		return {
+			images: ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"],
+		};
+	},
+	methods: {},
+}).mount("#app");
